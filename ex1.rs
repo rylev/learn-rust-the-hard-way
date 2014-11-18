6eb626a035e9f28fe35e688bce0b63356df18490
Ryan Levick <ryan@6wunderkinder.com>
2014-11-18 23:59:22 +0100

    Add example 1

---
+++ ex1.rs
@@ -0,0 +1,9 @@
+// Example 1
+// http://c.learncodethehardway.org/book/ex1.html
+
+// All Rust programs start with a main function
+fn main() {
+    // println! is a macro that prints a string to stdout. For now you can just
+    // think of macros as being the same as functions.
+    println!("Hello, World");
+}
