38ab84858ead1c59b63807e4f953239e530f575e
Ryan Levick <ryan@6wunderkinder.com>
2014-11-21 00:13:47 +0100

    Add some info on convertin String to &str

--- ex8.rs
+++ ex8.rs
@@ -36,7 +36,8 @@ fn main() {
 // 2.) Forget to initialize the second loop's i so that it retains the value
 //     from the end of the first loop. Now your second loop might or might not run.
 // 3.) Forget to do a i++ increment at the end of the loop and you get a "forever loop"
-// 4.) Use a while loop to copy the values from args into states.
+// 4.) Use a while loop to copy the values from args into states. You will have to
+//     convert the Strings in args to &str by using the .as_slice() method on String.
 // 5.) Make this copy loop never fail such that if there's too many args elements
 //     it won't put them all into states.
 // 6.) Research if you've really copied these strings. The answer may surprise and
