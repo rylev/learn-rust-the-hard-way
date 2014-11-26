a259ea9bff35a3b4442db9f5a3bbac7ddd5aca03
Ryan Levick <ryan@6wunderkinder.com>
2014-11-26 20:51:25 +0100

    Add ex13 and ex14

---
+++ ex13.rs
@@ -0,0 +1,9 @@
+// http://c.learncodethehardway.org/book/ex15.html
+
+fn main() {
+    // The entire introduction on pointers in Learn C The Hard Way uses pointers
+    // in a very unsafe (easy to access junk memory) way. In C this is normal.
+    // One of the biggest benefits of Rust is that the language protects you from
+    // such evils.
+    // TODO: Write a Rust specific guide on pointers.
+}
---
+++ ex14.rs
@@ -0,0 +1,42 @@
+// http://c.learncodethehardway.org/book/ex16.html
+
+struct Person {
+    name: &'static str,
+    age: int,
+    height: int,
+    weight: int
+}
+
+fn create_person(name: &'static str, age: int, height: int, weight: int) -> Box<Person> {
+    box Person {name: name, age: age, height: height, weight: weight}
+}
+
+
+impl Person {
+    fn print(&self) {
+        println!("Name: {}", self.name);
+        println!("\tAge: {}", self.age);
+        println!("\tHeight: {}", self.height);
+        println!("\tWeight: {}", self.weight);
+    }
+}
+
+fn main() {
+    let mut joe = create_person("Joe Alex", 32, 64, 140);
+    let mut frank = create_person("Frank Blank", 20, 72, 180);
+
+    println!("Joe is at memory location {:p}", &*joe as *const _);
+    joe.print();
+
+    println!("Frank is at memory location {:p}", &*frank as *const _);
+    frank.print();
+
+    joe.age += 20;
+    joe.height -= 2;
+    joe.weight += 40;
+    joe.print();
+
+    frank.age += 20;
+    frank.weight += 20;
+    frank.print();
+}
