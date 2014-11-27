997dc99ef8b7b3588c1a0f69efb8e8d5c3e5d964
Ryan Levick <ryan@6wunderkinder.com>
2014-11-28 00:40:22 +0100

    More database

--- ex15.rs
+++ ex15.rs
@@ -1,16 +1,20 @@
-use std::io::File;
-use std::io::FileMode::Open;
+use std::io::File; use std::io::FileMode::Open;
 use std::io::FileAccess::{Write,ReadWrite};
+use std::io::IoResult;
+use std::fmt::{Show,Formatter,Error};
+use std::io::SeekStyle;
 
 const MAX_DATA: uint = 512;
 const MAX_ROWS: uint = 100;
 
 
-type Field = [u8, ..MAX_DATA];
+struct Field {
+    field: [u8, ..MAX_DATA]
+}
 
 struct Row {
-    id: i64,
-    set: i64,
+    id: u64,
+    set: u64,
     name: Field,
     email: Field
 }
@@ -24,19 +28,65 @@ struct Connection {
     database: Database
 }
 
-const EMPTY_FIELD: Field = [0, ..MAX_DATA];
+const EMPTY_FIELD: Field = Field { field: [0, ..MAX_DATA] };
 const EMPTY_ROW: Row = Row {id: 0, set: 0, name: EMPTY_FIELD, email: EMPTY_FIELD};
 const EMPTY_DATABASE: Database = Database {rows: [EMPTY_ROW, ..MAX_ROWS]};
 
-//impl Address {
-//    fn print(&self) {
-//        print!("{} {} {}", self.id, self.name, self.email);
-//    }
-//}
+impl Show for Field {
+    fn fmt(&self, formater: &mut Formatter) -> Result<(), Error> {
+        // TODO: encode the field field of field
+        // http://doc.rust-lang.org/std/ascii/trait.AsciiCast.html
+        write!(formater, "FIXME!")
+    }
+}
+
+impl Row {
+    fn from_reader(&mut self, file: &mut File) -> IoResult<()> {
+        self.id = try!(file.read_le_u64());
+        self.set = try!(file.read_le_u64());
+        try!(file.read(self.name.field.as_mut_slice()));
+        try!(file.read(self.email.field.as_mut_slice()));
+        Ok(())
+    }
+
+    fn to_writer(&mut self, file: &mut File) -> IoResult<()> {
+        try!(file.write_le_u64(self.id));
+        try!(file.write_le_u64(self.set));
+        try!(file.write(self.name.field.as_slice()));
+        try!(file.write(self.email.field.as_slice()));
+        Ok(())
+    }
+
+    fn set(&mut self, name: &str, email: &str) {
+        if (self.set == 1) {
+            panic!("already set, must first delete");
+        }
+        self.set = 1;
+        // need to check the length of the string
+        let mut name_copy = [0,..512];
+        std::slice::bytes::copy_memory(&mut name_copy, name.as_bytes());
+        self.name = Field { field: name_copy };
+
+        let mut email_copy = [0,..512];
+        std::slice::bytes::copy_memory(&mut email_copy, email.as_bytes());
+        self.email = Field { field: email_copy };
+    }
+}
+
+impl Show for Row {
+    fn fmt(&self, formater: &mut Formatter) -> Result<(), Error> {
+        write!(formater, "<{}> {} {}", self.id, self.name, self.email)
+    }
+}
+
+impl Database {
+    fn set(&mut self, id: u64, name: &str, email: &str) {
+        self.rows[id as uint].set(email, email);
+    }
+}
 
 impl Connection {
     fn new(filename: &str, mode: &str) -> Connection {
-
         let path = Path::new(filename);
         if mode == "c" {
             let file = match File::open_mode(&path, Open, Write){
@@ -50,93 +100,35 @@ impl Connection {
                 Err(e) => panic!("file error: {}", e)
             };
 
-            let conn = Connection {file: file, database: EMPTY_DATABASE};
+            let mut conn = Connection {file: file, database: EMPTY_DATABASE};
             conn.load_database();
             conn
         }
     }
 
-    fn load_database(&self) {
-        //int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
-        //if(rc != 1) panic!("Failed to load database.");
+    fn load_database(&mut self) {
+        for row in self.database.rows.iter_mut() {
+            // use return value to know when to stop
+            row.from_reader(&mut self.file);
+        }
     }
 
-    fn create_database(&self) {
+    fn create_database(&mut self) {
+        for i in range(0, MAX_ROWS) {
+            for row in self.database.rows.iter_mut() {
+                row.id = i as u64;
+            }
+        }
     }
+
+    fn write_database(&mut self) {
+        self.file.seek(0, SeekStyle::SeekSet);
+        for row in self.database.rows.iter_mut() {
+            row.to_writer(&mut self.file);
+        }
+    }
+
 }
-
-
-//impl Database {
-//    fn get(&self, id: u8) {
-//        let addr = &db.rows[id];
-//
-//        if addr.set  {
-//            addr.print();
-//        } else {
-//            panic!("ID is not set");
-//        }
-//    }
-//}
-
-
-//void Database_write(struct Connection *conn)
-//{
-//    rewind(conn->file);
-//
-//    int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
-//    if(rc != 1) die("Failed to write database.");
-//
-//    rc = fflush(conn->file);
-//    if(rc == -1) die("Cannot flush database.");
-//}
-//
-//void Database_create(struct Connection *conn)
-//{
-//    int i = 0;
-//
-//    for(i = 0; i < MAX_ROWS; i++) {
-//            // make a prototype to initialize it
-//            struct Address addr = {.id = i, .set = 0};
-//            // then just assign it
-//            conn->db->rows[i] = addr;
-//        }
-//}
-//
-//void Database_set(struct Connection *conn, int id, const char *name, const char *email)
-//{
-//    struct Address *addr = &conn->db->rows[id];
-//    if(addr->set) die("Already set, delete it first");
-//
-//    addr->set = 1;
-//    // WARNING: bug, read the "How To Break It" and fix this
-//    char *res = strncpy(addr->name, name, MAX_DATA);
-//    // demonstrate the strncpy bug
-//    if(!res) die("Name copy failed");
-//
-//    res = strncpy(addr->email, email, MAX_DATA);
-//    if(!res) die("Email copy failed");
-//}
-//
-//
-//void Database_delete(struct Connection *conn, int id)
-//{
-//    struct Address addr = {.id = id, .set = 0};
-//    conn->db->rows[id] = addr;
-//}
-//
-//void Database_list(struct Connection *conn)
-//{
-//    int i = 0;
-//    struct Database *db = conn->db;
-//
-//    for(i = 0; i < MAX_ROWS; i++) {
-//            struct Address *cur = &db->rows[i];
-//
-//            if(cur->set) {
-//                        Address_print(cur);
-//                    }
-//        }
-//}
 
 fn main() {
     let argv = std::os::args();
@@ -146,53 +138,48 @@ fn main() {
         panic!("USAGE: ex15 <dbfile> <action> [action params]");
     }
 
-    let filename = &argv[1];
-    let action = &argv[2];
-    let conn = Connection::new(filename.as_slice(), action.as_slice());
+    let filename = argv[1].as_slice();
+    let action = argv[2].as_slice();
+    let mut conn = Connection::new(filename, action);
 
-    let id: uint = match from_str(argv[3].as_slice()) {
+    let id: u64 = match from_str(argv[3].as_slice()) {
         Some(i) => i,
         None => panic!("{} is not a number!")
     };
 
-    if id >= MAX_ROWS {
+    if id >= (MAX_ROWS as u64) {
         panic!("There's not that many records.");
     }
 
-    // Remove me!
-    conn.create_database();
-    //match action {
-    //    "c" => {
-    //        conn.create_database();
-    //        conn.write_database();
-    //    },
-    //    "g" => {
-    //        if argc != 4 { panic!("Need an id to get"); }
-    //        let database = &conn.database;
-    //        database.get(id);
-    //    },
-    //    "s" => {
-    //        if argc != 6 { panic!("Need id, name, email to set"); }
+    match action.as_slice() {
+        "c" => {
+            conn.create_database();
+            conn.write_database();
+        },
+        //"g" => {
+        //    if argc != 4 { panic!("Need an id to get"); }
+        //    let database = &conn.database;
+        //    database.get(id);
+        //},
+        "s" => {
+            if argc != 6 { panic!("Need id, name, email to set"); }
 
-    //        let database = &conn.database;
+            conn.database.set(id, argv[4].as_slice(), argv[5].as_slice());
+            conn.write_database();
+        },
 
-    //        database.set(id, argv[4], argv[5]);
-    //        conn.write_database();
-    //    },
+        //"d" => {
+        //    if argc != 4 { panic!("Need id to delete"); }
 
-    //    "d" => {
-    //        if argc != 4 { panic!("Need id to delete"); }
+        //    let database = &conn.database;
 
-    //        let database = &conn.database;
+        //    database.delete(id);
+        //    conn.write_database();
+        //},
+        //"l" => {
+        //    conn.list_database();
+        //},
+        _ => panic!("Invalid action, only: c=create, g=get, s=set, d=del, l=list")
+    }
 
-    //        database.delete(id);
-    //        conn.write_database();
-    //    },
-    //    "l" => {
-    //        conn.list_database();
-    //    },
-    //    _ => ("Invalid action, only: c=create, g=get, s=set, d=del, l=list")
-    //}
-
-    //conn.close_database();
 }
