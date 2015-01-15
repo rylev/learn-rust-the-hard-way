697f963534913aef9c0531aa189eecd51f33b87e
Ryan Levick <ryan@6wunderkinder.com>
2015-01-15 06:29:24 +0100

    Update example 3 to rust 1.0 alpha

--- ex3.rs
+++ ex3.rs
@@ -3,33 +3,33 @@
 fn main(){
     // Variable assignment is done using the 'let' keyword
     // No type annotations are neceesary as Rust has local type inference
-    // Here we are assigning the integer 10 to the variable age.
-    let age = 10i;
+    // Here we are assigning the 8 bit signed integer 10 to the variable age.
+    let age = 10i8;
 
     // Number literals (integers, unsigned integers, floats, etc.) are distinguished
     // by one letter after the number. We'll cover more literals later.
-    let height = 72i;
+    let height = 72i8;
 
     // You can skip annotating the literal if you give an explicit type
-    let shoe_size : int = 12;
+    let shoe_size : i8 = 12;
 
-    println!("I am {:d} years old.", age);
-    println!("I am {:d} inches tall.", height);
-    println!("I wear a size {:d} shoe.", shoe_size);
+    println!("I am {} years old.", age);
+    println!("I am {} inches tall.", height);
+    println!("I wear a size {} shoe.", shoe_size);
 
     // Variables are immutable by default. If you want to reassign a variable to
     // another value you must first declare it mutable using the 'mut' keyword.
     // TODO: Uncomment the following three lines and compile
     // Be warned this will emit a compiler warning.
 
-    // let mut european_shoe_size = 42i;
-    // european_shoe_size = 46i;
-    // println!("I wear a size {:d} shoe in Europe.", european_shoe_size);
+    // let mut european_shoe_size = 42i8;
+    // european_shoe_size = 46i8;
+    // println!("I wear a size {} shoe in Europe.", european_shoe_size);
 }
 
 // Questions and Exercises:
 // 1.) Are the semicolons necessary?
-// 2.) What happens when you replace the '{:d}' with '{:k}'?
+// 2.) What happens when you replace the '{}' with '{:k}'?
 // 3.) What happens when you try to reassign a variable not declared mutable?
 // 4.) Get the compiler to compile the mutable variable reassignment code without a
 //     compiler warning.
